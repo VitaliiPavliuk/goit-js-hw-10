f2ab3021cb6d1b88a903192b33141af2435994df
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-01-28 20:29:04 +0200

    Add fetch and debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const inputField = document.querySelector('input#search-box');
+
+const onInput = event => {
+  const country = event.target.value.trim();
+  console.log(fetchCountries(country));
+};
+
+inputField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
